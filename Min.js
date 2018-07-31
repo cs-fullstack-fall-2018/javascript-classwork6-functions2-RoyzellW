43df9e2f7875f6fb87a6d522685755135b488a0f
student <royzellwalton@gmail.com>
2018-07-31 13:25:00 -0500

    Number 16

--- Min.js
+++ Min.js
@@ -1,18 +1,21 @@
 function main() {
 
     var userInput = parseInt(prompt("Enter your favorite number: "));
+    aboveOrBelow100(userInput);
 }
 
 main();
+
 
 function aboveOrBelow100(userInput) {
     if (userInput < 100) {
         alert("Below 100!");
     }
-    else if (userInput == 100) {
+    else if (userInput === 100) {
         alert("Equals 100!")
     }
 }
+
 
 
 
